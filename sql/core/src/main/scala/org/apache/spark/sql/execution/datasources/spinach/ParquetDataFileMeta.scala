b8f16028a9859582f6dcf08750f928ffcb6061b0
LuciferYang <valenti.y.29@gmail.com>
2016-11-11 16:18:11 +0800

    add ParquetDataFileMeta

--- sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/spinach/ParquetDataFileMeta.scala
+++ sql/core/src/main/scala/org/apache/spark/sql/execution/datasources/spinach/ParquetDataFileMeta.scala
@@ -1,8 +1,47 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.apache.spark.sql.execution.datasources.spinach
 
-/**
-  * Created by yangjie on 2016/11/11.
-  */
-class ParquetDataFileMeta {
+import java.util.{List => JList}
+
+import org.apache.hadoop.conf.Configuration
+import org.apache.hadoop.fs.{FSDataInputStream, Path}
+import org.apache.parquet.format.converter.ParquetMetadataConverter._
+import org.apache.parquet.hadoop.ParquetFileReader
+import org.apache.parquet.hadoop.metadata.{BlockMetaData, FileMetaData}
+
+
+private[spinach] class ParquetDataFileMeta(var meta: FileMetaData,
+                 var blocks: JList[BlockMetaData]) extends DataFileHandle {
+
+  override def fin: FSDataInputStream = null
+
+  override def len: Long = 0
+
+  def this() = {
+    this(null, null)
+  }
+
+  def read(conf: Configuration, path: Path): ParquetDataFileMeta = {
+    val footer = ParquetFileReader.readFooter(conf, path, NO_FILTER)
+    this.meta = footer.getFileMetaData
+    this.blocks = footer.getBlocks
+    this
+  }
 
 }
