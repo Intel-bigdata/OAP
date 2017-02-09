d4342590246025665711f3dddc275d90e9fb8759
LuciferYang <valenti.y.29@gmail.com>
2017-02-09 15:20:59 +0800

    clean code

---
+++ sql/core/src/main/java/org/apache/parquet/io/RecordReaderFactory.java
@@ -0,0 +1,35 @@
+package org.apache.parquet.io;
+
+
+import org.apache.parquet.column.impl.ColumnReadStoreImpl;
+import org.apache.parquet.column.page.PageReadStore;
+import org.apache.parquet.io.api.RecordMaterializer;
+
+import java.util.List;
+
+import static org.apache.parquet.Preconditions.checkNotNull;
+
+public class RecordReaderFactory {
+
+    public static <T> RecordReader<T> getRecordReader(MessageColumnIO root, PageReadStore columns,
+                                               RecordMaterializer<T> recordMaterializer,
+                                               String createdBy,
+                                               List<Long> rowIdList) {
+        checkNotNull(root, "messageColumnIO");
+        checkNotNull(columns, "columns");
+        checkNotNull(recordMaterializer, "recordMaterializer");
+
+        List<PrimitiveColumnIO> leaves = root.getLeaves();
+
+        if (leaves.isEmpty()) {
+            return new EmptyRecordReader<>(recordMaterializer);
+        }
+
+        return new PositionableRecordReaderImpl<>(
+                root,
+                recordMaterializer,
+                new ColumnReadStoreImpl(columns, recordMaterializer.getRootConverter(), root.getType(), createdBy),
+                columns.getRowCount(),
+                rowIdList);
+    }
+}
