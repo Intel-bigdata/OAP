c013dae0d8be7a43269e3f5c3d37beaad81d3e0c
Linhong Liu <linhong.liu@intel.com>
2017-06-29 12:07:58 +0800

    Fix bugs in Index Statistics for Multi-Column case
    In Multi-Column case, an interval has more than one columns and the last one
    maybe DUMMY_KEY. For example interval.start = (1, 100, "a", DUMMY_KEY_START)
    But, interval.start != DUMMY_KEY_START, and compare it may still encounter
    ArrayIndexOutOfBoundException. This PR fixes this problem

--- src/test/scala/org/apache/spark/sql/execution/datasources/oap/FilterSuite.scala
+++ src/test/scala/org/apache/spark/sql/execution/datasources/oap/FilterSuite.scala
@@ -135,6 +135,28 @@ class FilterSuite extends QueryTest with SharedSQLContext with BeforeAndAfterEac
     sql("drop oindex index1 on oap_test")
   }
 
+  test("filtering multi index") {
+    sqlContext.conf.setConf(SQLConf.SPINACH_STATISTICS_TYPES, "PARTBYVALUE")
+    val data: Seq[(Int, String)] = (1 to 300).map { i => (i, s"this is test $i") }
+    data.toDF("key", "value").createOrReplaceTempView("t")
+    sql("insert overwrite table spinach_test select * from t")
+    sql("create sindex index1 on spinach_test (a, b)")
+
+    checkAnswer(sql("SELECT * FROM spinach_test WHERE a = 150 and b < 'this is test 3'"),
+      Row(150, "this is test 150") :: Nil)
+
+    checkAnswer(sql("SELECT * FROM spinach_test WHERE a = 150 and b >= 'this is test 1'"),
+      Row(150, "this is test 150") :: Nil)
+
+    checkAnswer(sql("SELECT * FROM spinach_test WHERE a = 150 and b = 'this is test 150'"),
+      Row(150, "this is test 150") :: Nil)
+
+    checkAnswer(sql("SELECT * FROM spinach_test WHERE a > 299 and b < 'this is test 9'"),
+      Row(300, "this is test 300") :: Nil)
+
+    sql("drop sindex index1 on spinach_test")
+  }
+
   test("filtering parquet") {
     val data: Seq[(Int, String)] = (1 to 300).map { i => (i, s"this is test $i") }
     data.toDF("key", "value").createOrReplaceTempView("t")
