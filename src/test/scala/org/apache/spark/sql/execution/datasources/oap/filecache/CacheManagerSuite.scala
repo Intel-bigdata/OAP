a5fb45ed37466a8475392d2e17129f9c9bd32d82
Linhong Liu <linhong.liu@intel.com>
2017-11-24 10:31:23 +0800

    workaround for test fail
    CacheManager and MemoryManager can't be initilized twice during
    test. So allocate same off-heap memory size for all test suites.

--- src/test/scala/org/apache/spark/sql/execution/datasources/oap/filecache/CacheManagerSuite.scala
+++ src/test/scala/org/apache/spark/sql/execution/datasources/oap/filecache/CacheManagerSuite.scala
@@ -35,51 +35,51 @@ class CacheManagerSuite extends SparkFunSuite {
     new SparkContext(
       "local[2]",
       "MemoryManagerSuite",
-      new SparkConf().set("spark.memory.offHeap.size", "10k"))
+      new SparkConf().set("spark.memory.offHeap.size", "2k"))
 
     val configuration = new Configuration()
 
-    val data1024 = generateData(1024)
-    val data2048 = generateData(2048)
-    val data4096 = generateData(4096)
-    val data7168 = generateData(7168)
-    val data10240 = generateData(10240)
+    val data1 = generateData(256)
+    val data2 = generateData(512)
+    val data3 = generateData(665)
+    val data4 = generateData(1433)
+    val data5 = generateData(10240)
 
-    val fiber1024 = TestFiber(() => data1024, s"test fiber #1, size 1024")
-    val fiber2048 = TestFiber(() => data2048, s"test fiber #2, size 2048")
-    val fiber4096 = TestFiber(() => data4096, s"test fiber #3, size 4096")
-    val fiber7168 = TestFiber(() => data7168, s"test fiber #4, size 7168")
-    val fiber10240 = TestFiber(() => data10240, s"test fiber #5, size 10240")
+    val fiber1 = TestFiber(() => data1, s"test fiber #1, size 256")
+    val fiber2 = TestFiber(() => data2, s"test fiber #2, size 512")
+    val fiber3 = TestFiber(() => data3, s"test fiber #3, size 665")
+    val fiber4 = TestFiber(() => data4, s"test fiber #4, size 1433")
+    val fiber5 = TestFiber(() => data5, s"test fiber #5, size 10240")
 
     // Put 3 fibers into empty cache
-    CacheManager.getOrElseUpdate(fiber1024, configuration)
-    CacheManager.getOrElseUpdate(fiber2048, configuration)
-    CacheManager.getOrElseUpdate(fiber4096, configuration)
+    CacheManager.getOrElseUpdate(fiber1, configuration)
+    CacheManager.getOrElseUpdate(fiber2, configuration)
+    CacheManager.getOrElseUpdate(fiber3, configuration)
     assert(CacheManager.getMissCount == 3)
 
     // Get 3 fibers from cache
-    CacheManager.getOrElseUpdate(fiber1024, configuration)
-    CacheManager.getOrElseUpdate(fiber2048, configuration)
-    CacheManager.getOrElseUpdate(fiber4096, configuration)
+    CacheManager.getOrElseUpdate(fiber1, configuration)
+    CacheManager.getOrElseUpdate(fiber2, configuration)
+    CacheManager.getOrElseUpdate(fiber3, configuration)
     assert(CacheManager.getHitCount == 3)
 
     // Put 1 large fiber into cache will cause eviction
-    CacheManager.getOrElseUpdate(fiber7168, configuration)
+    CacheManager.getOrElseUpdate(fiber4, configuration)
     assert(CacheManager.getMissCount == 4)
     // Get large fiber from cache
-    CacheManager.getOrElseUpdate(fiber7168, configuration)
+    CacheManager.getOrElseUpdate(fiber4, configuration)
     assert(CacheManager.getHitCount == 4)
 
     // Get 3 removed fibers from cache
-    CacheManager.getOrElseUpdate(fiber1024, configuration)
-    CacheManager.getOrElseUpdate(fiber2048, configuration)
-    CacheManager.getOrElseUpdate(fiber4096, configuration)
+    CacheManager.getOrElseUpdate(fiber1, configuration)
+    CacheManager.getOrElseUpdate(fiber2, configuration)
+    CacheManager.getOrElseUpdate(fiber3, configuration)
     assert(CacheManager.getMissCount == 7)
 
     // Put too large fiber into cache, will not remove other fibers
-    CacheManager.getOrElseUpdate(fiber10240, configuration)
+    CacheManager.getOrElseUpdate(fiber5, configuration)
     assert(CacheManager.getMissCount == 8)
-    CacheManager.getOrElseUpdate(fiber2048, configuration)
+    CacheManager.getOrElseUpdate(fiber2, configuration)
     assert(CacheManager.getMissCount == 8)
   }
 }
