1cf595a35752938857825b9e336d0274778d9a4b
LuciferYang <valenti.y.29@gmail.com>
2017-12-01 11:39:57 +0800

    change conf name

--- src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
+++ src/main/scala/org/apache/spark/sql/internal/SQLConf.scala
@@ -729,7 +729,7 @@ object SQLConf {
       .createWithDefault(0.7)
 
   val OAP_INDEXER_CHOICE_MAX_SIZE =
-    SQLConfigBuilder("spark.sql.oap.indexer.max.size")
+    SQLConfigBuilder("spark.sql.oap.indexer.max.use.size")
       .internal()
       .doc("The max availabe indexer choose size.")
       .intConf
