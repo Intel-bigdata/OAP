b1791c571cd48ab9e0c2ec0590a1eaf3e45ed94b
LuciferYang <valenti.y.29@gmail.com>
2018-02-15 11:36:16 +0800

    format ParquetDataFile.scala

--- src/main/scala/org/apache/spark/sql/execution/datasources/oap/io/ParquetDataFile.scala
+++ src/main/scala/org/apache/spark/sql/execution/datasources/oap/io/ParquetDataFile.scala
@@ -33,8 +33,10 @@ import org.apache.spark.sql.execution.datasources.parquet.ParquetReadSupportHelp
 import org.apache.spark.sql.types.StructType
 
 
-private[oap] case class ParquetDataFile(path: String, schema: StructType,
-                                        configuration: Configuration) extends DataFile {
+private[oap] case class ParquetDataFile(
+    path: String,
+    schema: StructType,
+    configuration: Configuration) extends DataFile {
 
   def getFiberData(groupId: Int, fiberId: Int, conf: Configuration): FiberCache = {
     // TODO data cache
