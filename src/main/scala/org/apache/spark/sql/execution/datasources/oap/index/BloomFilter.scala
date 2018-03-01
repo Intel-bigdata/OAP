dc3e266800932acd162200cab61861c24e927b0f
Carson Wang <carson.wang@intel.com>
2018-03-01 10:06:01 +0800

    update checkExist

--- src/main/scala/org/apache/spark/sql/execution/datasources/oap/index/BloomFilter.scala
+++ src/main/scala/org/apache/spark/sql/execution/datasources/oap/index/BloomFilter.scala
@@ -48,12 +48,7 @@ class BloomFilter(
   }
 
   def checkExist(value: String): Boolean = {
-    val indices = getIndices(value)
-    for (i <- indices)
-      if (!bloomBitSet.contains(i)) {
-        return false
-      }
-    true
+    getIndices(value).forall(bloomBitSet.contains)
   }
 
   def addValue(data: Array[Byte]): Unit = {
