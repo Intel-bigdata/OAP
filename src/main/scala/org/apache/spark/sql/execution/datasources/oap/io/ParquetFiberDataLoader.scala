a83d0a20e35c6eb34826583cd1abb037ef2d1745
yangjie01 <yangjie01@baidu.com>
2018-08-30 13:28:21 +0800

    fix format

--- src/main/scala/org/apache/spark/sql/execution/datasources/oap/io/ParquetFiberDataLoader.scala
+++ src/main/scala/org/apache/spark/sql/execution/datasources/oap/io/ParquetFiberDataLoader.scala
@@ -28,7 +28,7 @@ import org.apache.parquet.schema.{MessageType, Type}
 import org.apache.spark.memory.MemoryMode
 import org.apache.spark.sql.execution.datasources.OapException
 import org.apache.spark.sql.execution.datasources.oap.filecache.FiberCache
-import org.apache.spark.sql.execution.datasources.parquet.{SkippableVectorizedColumnReader, ParquetReadSupportWrapper}
+import org.apache.spark.sql.execution.datasources.parquet.{ParquetReadSupportWrapper, SkippableVectorizedColumnReader}
 import org.apache.spark.sql.execution.vectorized.{ColumnVector, OnHeapColumnVector, OnHeapColumnVectorFiber}
 import org.apache.spark.sql.oap.OapRuntime
 import org.apache.spark.sql.types._
