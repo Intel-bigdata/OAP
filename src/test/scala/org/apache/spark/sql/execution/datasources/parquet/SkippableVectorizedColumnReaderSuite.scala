dde44c900cd66a2223a16b34e6b62a42e5550a34
yangjie01 <yangjie01@baidu.com>
2018-09-03 17:25:27 +0800

    add test case

--- src/test/scala/org/apache/spark/sql/execution/datasources/parquet/SkippableVectorizedColumnReaderSuite.scala
+++ src/test/scala/org/apache/spark/sql/execution/datasources/parquet/SkippableVectorizedColumnReaderSuite.scala
@@ -22,22 +22,23 @@ import java.io.File
 import org.apache.hadoop.fs.Path
 import org.apache.parquet.column.ParquetProperties.WriterVersion.PARQUET_1_0
 import org.apache.parquet.example.data.Group
-import org.apache.parquet.example.data.simple.SimpleGroupFactory
+import org.apache.parquet.example.data.simple.{NanoTime, SimpleGroupFactory}
 import org.apache.parquet.hadoop.OapParquetFileReader
 import org.apache.parquet.hadoop.example.{ExampleParquetWriter, GroupWriteSupport}
 import org.apache.parquet.hadoop.metadata.CompressionCodecName.UNCOMPRESSED
+import org.apache.parquet.io.api.Binary
 import org.apache.parquet.schema.{MessageType, PrimitiveType}
 import org.apache.parquet.schema.PrimitiveType.PrimitiveTypeName._
 import org.apache.parquet.schema.Type.Repetition.REQUIRED
 import org.scalatest.BeforeAndAfterEach
-
 import org.apache.spark.SparkFunSuite
+
 import org.apache.spark.internal.Logging
 import org.apache.spark.memory.MemoryMode
 import org.apache.spark.sql.execution.vectorized.ColumnVector
 import org.apache.spark.sql.internal.SQLConf
 import org.apache.spark.sql.test.oap.SharedOapContext
-import org.apache.spark.sql.types.{BooleanType, DataType, LongType, StructField, StructType}
+import org.apache.spark.sql.types._
 import org.apache.spark.util.Utils
 
 class SkippableVectorizedColumnReaderSuite extends SparkFunSuite with SharedOapContext
@@ -96,7 +97,211 @@ class SkippableVectorizedColumnReaderSuite extends SparkFunSuite with SharedOapC
     }
   }
 
-  test("skip And read longs") {
+  test("skip And read int32") {
+    // write parquet data, type is int
+    val parquetSchema: MessageType = new MessageType("test",
+      new PrimitiveType(REQUIRED, INT32, "int32_field")
+    )
+    val data: Seq[Group] = {
+      val factory = new SimpleGroupFactory(parquetSchema)
+      (0 until unitSize * 2).map(i => factory.newGroup()
+        .append("int32_field", i)
+      )
+    }
+    writeData(parquetSchema, data)
+
+    // skip and read data to ColumnVector
+    val columnVector = skipAndReadToVector(parquetSchema, IntegerType)
+
+    // assert result
+    (0 until unitSize).foreach { i =>
+      val actual = columnVector.getInt(i)
+      val excepted = i + unitSize
+      assert(actual == excepted)
+    }
+  }
+
+  test("skip And read int32 with dic") {
+    // write parquet data, type is int and with dic
+    val parquetSchema: MessageType = new MessageType("test",
+      new PrimitiveType(REQUIRED, INT32, "int32_field")
+    )
+    val data: Seq[Group] = {
+      val factory = new SimpleGroupFactory(parquetSchema)
+      (0 until unitSize * 2).map(i =>
+        if (i < unitSize) factory.newGroup().append("int32_field", 1)
+        else factory.newGroup().append("int32_field", 2)
+      )
+    }
+    writeData(parquetSchema, data)
+
+    // skip and read data to ColumnVector
+    val columnVector = skipAndReadToVector(parquetSchema, IntegerType)
+
+    // assert result
+    (0 until unitSize).foreach { i =>
+      val actual = columnVector.getInt(i)
+      val excepted = 2
+      assert(actual == excepted)
+    }
+  }
+
+  test("skip int32 and throw UnsupportedOperation") {
+    // write parquet data, type is int
+    val parquetSchema: MessageType = new MessageType("test",
+      new PrimitiveType(REQUIRED, INT32, "int32_field")
+    )
+    val data: Seq[Group] = {
+      val factory = new SimpleGroupFactory(parquetSchema)
+      (0 until unitSize * 2).map(i => factory.newGroup()
+        .append("int32_field", i)
+      )
+    }
+    writeData(parquetSchema, data)
+
+    // skip with wrong type
+    intercept[UnsupportedOperationException] {
+      skipAndThrowUnsupportedOperation(parquetSchema, BooleanType)
+    }
+  }
+
+  test("skip And read float") {
+    // write parquet data, type is float
+    val parquetSchema: MessageType = new MessageType("test",
+      new PrimitiveType(REQUIRED, FLOAT, "float_field")
+    )
+    val data: Seq[Group] = {
+      val factory = new SimpleGroupFactory(parquetSchema)
+      (0 until unitSize * 2).map(i => factory.newGroup()
+        .append("float_field", i.toFloat)
+      )
+    }
+    writeData(parquetSchema, data)
+
+    // skip and read data to ColumnVector
+    val columnVector = skipAndReadToVector(parquetSchema, FloatType)
+
+    // assert result
+    (0 until unitSize).foreach { i =>
+      val actual = columnVector.getFloat(i)
+      val excepted = (i + unitSize).toFloat
+      assert(actual == excepted)
+    }
+  }
+
+  test("skip And read float with dic") {
+    // write parquet data, type is float and with dic
+    val parquetSchema: MessageType = new MessageType("test",
+      new PrimitiveType(REQUIRED, FLOAT, "float_field")
+    )
+    val data: Seq[Group] = {
+      val factory = new SimpleGroupFactory(parquetSchema)
+      (0 until unitSize * 2).map(i =>
+        if (i < unitSize) factory.newGroup().append("float_field", 1F)
+        else factory.newGroup().append("float_field", 2F)
+      )
+    }
+    writeData(parquetSchema, data)
+
+    // skip and read data to ColumnVector
+    val columnVector = skipAndReadToVector(parquetSchema, FloatType)
+
+    // assert result
+    (0 until unitSize).foreach { i =>
+      val actual = columnVector.getFloat(i)
+      val excepted = 2F
+      assert(actual == excepted)
+    }
+  }
+
+  test("skip float and throw UnsupportedOperation") {
+    // write parquet data, type is float
+    val parquetSchema: MessageType = new MessageType("test",
+      new PrimitiveType(REQUIRED, FLOAT, "float_field")
+    )
+    val data: Seq[Group] = {
+      val factory = new SimpleGroupFactory(parquetSchema)
+      (0 until unitSize * 2).map(i => factory.newGroup()
+        .append("float_field", i.toFloat)
+      )
+    }
+    writeData(parquetSchema, data)
+
+    // skip with wrong type
+    intercept[UnsupportedOperationException] {
+      skipAndThrowUnsupportedOperation(parquetSchema, BooleanType)
+    }
+  }
+
+  test("skip And read double") {
+    // write parquet data, type is double
+    val parquetSchema: MessageType = new MessageType("test",
+      new PrimitiveType(REQUIRED, DOUBLE, "double_field")
+    )
+    val data: Seq[Group] = {
+      val factory = new SimpleGroupFactory(parquetSchema)
+      (0 until unitSize * 2).map(i => factory.newGroup()
+        .append("double_field", i.toDouble)
+      )
+    }
+    writeData(parquetSchema, data)
+
+    // skip and read data to ColumnVector
+    val columnVector = skipAndReadToVector(parquetSchema, DoubleType)
+
+    // assert result
+    (0 until unitSize).foreach { i =>
+      val actual = columnVector.getDouble(i)
+      val excepted = (i + unitSize).toDouble
+      assert(actual == excepted)
+    }
+  }
+
+  test("skip And read double with dic") {
+    // write parquet data, type is double and with dic
+    val parquetSchema: MessageType = new MessageType("test",
+      new PrimitiveType(REQUIRED, DOUBLE, "double_field")
+    )
+    val data: Seq[Group] = {
+      val factory = new SimpleGroupFactory(parquetSchema)
+      (0 until unitSize * 2).map(i =>
+        if (i < unitSize) factory.newGroup().append("double_field", 1D)
+        else factory.newGroup().append("double_field", 2D)
+      )
+    }
+    writeData(parquetSchema, data)
+
+    // skip and read data to ColumnVector
+    val columnVector = skipAndReadToVector(parquetSchema, DoubleType)
+
+    // assert result
+    (0 until unitSize).foreach { i =>
+      val actual = columnVector.getDouble(i)
+      val excepted = 2D
+      assert(actual == excepted)
+    }
+  }
+
+  test("skip double and throw UnsupportedOperation") {
+    // write parquet data, type is double
+    val parquetSchema: MessageType = new MessageType("test",
+      new PrimitiveType(REQUIRED, DOUBLE, "double_field")
+    )
+    val data: Seq[Group] = {
+      val factory = new SimpleGroupFactory(parquetSchema)
+      (0 until unitSize * 2).map(i => factory.newGroup()
+        .append("double_field", i.toDouble)
+      )
+    }
+    writeData(parquetSchema, data)
+
+    // skip with wrong type
+    intercept[UnsupportedOperationException] {
+      skipAndThrowUnsupportedOperation(parquetSchema, BooleanType)
+    }
+  }
+
+  test("skip And read int64") {
     // write parquet data, type is long
     val parquetSchema: MessageType = new MessageType("test",
       new PrimitiveType(REQUIRED, INT64, "int64_field")
@@ -120,7 +325,7 @@ class SkippableVectorizedColumnReaderSuite extends SparkFunSuite with SharedOapC
     }
   }
 
-  test("skip And read longs with dic") {
+  test("skip And read int64 with dic") {
     // write parquet data, type is long and with dic
     val parquetSchema: MessageType = new MessageType("test",
       new PrimitiveType(REQUIRED, INT64, "int64_field")
@@ -145,7 +350,7 @@ class SkippableVectorizedColumnReaderSuite extends SparkFunSuite with SharedOapC
     }
   }
 
-  test("skip longs and throw UnsupportedOperation") {
+  test("skip int64 and throw UnsupportedOperation") {
     // write parquet data, type is long
     val parquetSchema: MessageType = new MessageType("test",
       new PrimitiveType(REQUIRED, INT64, "int64_field")
@@ -154,6 +359,171 @@ class SkippableVectorizedColumnReaderSuite extends SparkFunSuite with SharedOapC
       val factory = new SimpleGroupFactory(parquetSchema)
       (0 until unitSize * 2).map(i => factory.newGroup()
         .append("int64_field", i.toLong)
+      )
+    }
+    writeData(parquetSchema, data)
+
+    // skip with wrong type
+    intercept[UnsupportedOperationException] {
+      skipAndThrowUnsupportedOperation(parquetSchema, BooleanType)
+    }
+  }
+
+  test("skip And read int96 with dic") {
+    // write parquet data, type is int96, actually int96 always has dic ...
+    val times = new Array[NanoTime](unitSize *2)
+    val parquetSchema: MessageType = new MessageType("test",
+      new PrimitiveType(REQUIRED, INT96, "int96_field")
+    )
+    val data: Seq[Group] = {
+      val factory = new SimpleGroupFactory(parquetSchema)
+      (0 until unitSize * 2).map(i => {
+        val time = new NanoTime(i, i.toLong)
+        times(i) = time
+        factory.newGroup().append("int96_field", time)
+      })
+    }
+    writeData(parquetSchema, data)
+
+    // skip and read data to ColumnVector
+    val columnVector = skipAndReadToVector(parquetSchema, TimestampType)
+
+    // assert result
+    (0 until unitSize).foreach { i =>
+      val actual = columnVector.getLong(i)
+      val excepted = ParquetRowConverter.binaryToSQLTimestamp(times(i + unitSize).toBinary)
+      assert(actual == excepted)
+    }
+  }
+
+  test("skip And read string") {
+    // write parquet data, type is long
+    val parquetSchema: MessageType = new MessageType("test",
+      new PrimitiveType(REQUIRED, BINARY, "string_field")
+    )
+    val data: Seq[Group] = {
+      val factory = new SimpleGroupFactory(parquetSchema)
+      (0 until unitSize * 2).map(i => factory.newGroup()
+        .append("string_field", String.valueOf(i))
+      )
+    }
+    writeData(parquetSchema, data)
+
+    // skip and read data to ColumnVector
+    val columnVector = skipAndReadToVector(parquetSchema, BinaryType)
+
+    // assert result
+    (0 until unitSize).foreach { i =>
+      val actual = columnVector.getUTF8String(i).toString
+      val excepted = String.valueOf(i + unitSize)
+      assert(actual == excepted)
+    }
+  }
+
+  test("skip And read string with dic") {
+    // write parquet data, type is long and with dic
+    val parquetSchema: MessageType = new MessageType("test",
+      new PrimitiveType(REQUIRED, BINARY, "string_field")
+    )
+    val data: Seq[Group] = {
+      val factory = new SimpleGroupFactory(parquetSchema)
+      (0 until unitSize * 2).map(i =>
+        if (i < unitSize) factory.newGroup().append("string_field", String.valueOf(1))
+        else factory.newGroup().append("string_field", String.valueOf(2))
+      )
+    }
+    writeData(parquetSchema, data)
+
+    // skip and read data to ColumnVector
+    val columnVector = skipAndReadToVector(parquetSchema, BinaryType)
+
+    // assert result
+    (0 until unitSize).foreach { i =>
+      val actual = columnVector.getUTF8String(i).toString
+      val excepted = String.valueOf(2)
+      assert(actual == excepted)
+    }
+  }
+
+  test("skip string and throw UnsupportedOperation") {
+    // write parquet data, type is long
+    val parquetSchema: MessageType = new MessageType("test",
+      new PrimitiveType(REQUIRED, BINARY, "string_field")
+    )
+    val data: Seq[Group] = {
+      val factory = new SimpleGroupFactory(parquetSchema)
+      (0 until unitSize * 2).map(i => factory.newGroup()
+        .append("string_field", String.valueOf(i))
+      )
+    }
+    writeData(parquetSchema, data)
+
+    // skip with wrong type
+    intercept[UnsupportedOperationException] {
+      skipAndThrowUnsupportedOperation(parquetSchema, BooleanType)
+    }
+  }
+
+  test("skip And read binary") {
+    // write parquet data, type is long
+    val parquetSchema: MessageType = new MessageType("test",
+      new PrimitiveType(REQUIRED, BINARY, "binary_field")
+    )
+    val data: Seq[Group] = {
+      val factory = new SimpleGroupFactory(parquetSchema)
+      (0 until unitSize * 2).map(i => factory.newGroup()
+        .append("binary_field", Binary.fromCharSequence(String.valueOf(i)))
+      )
+    }
+    writeData(parquetSchema, data)
+
+    // skip and read data to ColumnVector
+    val columnVector = skipAndReadToVector(parquetSchema, BinaryType)
+
+    // assert result
+    (0 until unitSize).foreach { i =>
+      val actual = columnVector.getBinary(i)
+      val excepted = String.valueOf(i + unitSize).getBytes
+      assert(actual.sameElements(excepted))
+    }
+  }
+
+  test("skip And read binary with dic") {
+    // write parquet data, type is long and with dic
+    val v1 = Binary.fromCharSequence("1")
+    val v2 = Binary.fromCharSequence("2")
+    val parquetSchema: MessageType = new MessageType("test",
+      new PrimitiveType(REQUIRED, BINARY, "binary_field")
+    )
+    val data: Seq[Group] = {
+      val factory = new SimpleGroupFactory(parquetSchema)
+      (0 until unitSize * 2).map(i =>
+        if (i < unitSize) factory.newGroup().append("binary_field", v1)
+        else factory.newGroup().append("binary_field", v2)
+      )
+    }
+    writeData(parquetSchema, data)
+
+    // skip and read data to ColumnVector
+    val columnVector = skipAndReadToVector(parquetSchema, BinaryType)
+
+    // assert result
+    (0 until unitSize).foreach { i =>
+      val actual = columnVector.getBinary(i)
+      val excepted = v2.getBytes
+      assert(actual.sameElements(excepted))
+    }
+  }
+
+  test("skip binary and throw UnsupportedOperation") {
+    // write parquet data, type is binary
+    val parquetSchema: MessageType = new MessageType("test",
+      new PrimitiveType(REQUIRED, BINARY, "binary_field")
+    )
+    val data: Seq[Group] = {
+      val factory = new SimpleGroupFactory(parquetSchema)
+      (0 until unitSize * 2).map(i => factory.newGroup()
+        .append("binary_field", Binary.fromCharSequence(String.valueOf(i)))
       )
     }
     writeData(parquetSchema, data)
@@ -182,7 +552,8 @@ class SkippableVectorizedColumnReaderSuite extends SparkFunSuite with SharedOapC
 
   private def skipAndThrowUnsupportedOperation(
       parquetSchema: MessageType,
-      dataType: DataType): Unit = {
+      dataType: DataType,
+      isArray: Boolean = false): Unit = {
     val footer = OapParquetFileReader
       .readParquetFooter(configuration, new Path(fileName)).toParquetMetadata
     val reader = OapParquetFileReader.open(configuration, new Path(fileName), footer)
@@ -191,7 +562,7 @@ class SkippableVectorizedColumnReaderSuite extends SparkFunSuite with SharedOapC
     val columnDescriptor = parquetSchema.getColumns.get(0)
     val pageReader = rowGroup.getPageReader(columnDescriptor)
     val columnReader = new SkippableVectorizedColumnReader(columnDescriptor, pageReader)
-    columnReader.skipBatch(unitSize, dataType, false)
+    columnReader.skipBatch(unitSize, dataType, isArray)
   }
 
   private def writeData(writeSchema: MessageType, data: Seq[Group]): Unit = {
