f9409b39f8f5aaca22ada78a1c3b391d648070b4
yangjie01 <yangjie01@baidu.com>
2018-10-26 13:06:52 +0800

    fix import

--- src/test/spark2.3/scala/org/apache/spark/sql/execution/datasources/oap/io/ParquetDataFileSuite.scala
+++ src/test/spark2.3/scala/org/apache/spark/sql/execution/datasources/oap/io/ParquetDataFileSuite.scala
@@ -23,6 +23,7 @@ import java.util.TimeZone
 import scala.collection.mutable.ArrayBuffer
 
 import java.util
+
 import org.apache.hadoop.conf.Configuration
 import org.apache.hadoop.fs.Path
 import org.apache.parquet.column.ColumnDescriptor
