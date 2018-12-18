06e4d5b2aa9b399c33056c64262ee93f31197c73
mengbo <mengbo@baidu.com>
2018-12-18 15:46:59 +0800

    optimize import in scala files

--- src/main/spark2.3.2/scala/org/apache/spark/sql/execution/datasources/DataSource.scala
+++ src/main/spark2.3.2/scala/org/apache/spark/sql/execution/datasources/DataSource.scala
@@ -31,6 +31,7 @@ import org.apache.spark.internal.Logging
 import org.apache.spark.sql._
 import org.apache.spark.sql.catalyst.analysis.UnresolvedAttribute
 import org.apache.spark.sql.catalyst.catalog.{BucketSpec, CatalogStorageFormat, CatalogTable, CatalogUtils}
+import org.apache.spark.sql.catalyst.expressions.Attribute
 import org.apache.spark.sql.catalyst.plans.logical.LogicalPlan
 import org.apache.spark.sql.catalyst.util.CaseInsensitiveMap
 import org.apache.spark.sql.execution.SparkPlan
