fe79f61ac25737c2df9e6344f33a7d53e8b25d21
Jiayi Chen <jiayi.chen@intel.com>
2020-05-29 10:28:50 +0800

    Enable mvn package to build native libs

---
+++ oap-native-sql/cpp/compile.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+
+CURRENT_DIR=$(cd "$(dirname "$BASH_SOURCE")"; pwd)
+echo $CURRENT_DIR
+
+cd ${CURRENT_DIR}
+if [ -d build ]; then
+    rm -r build
+fi
+mkdir build
+cd build
+cmake ..
+make -j
+make -j
+make install
+
+# cd $CURRENT_DIR
+# rm -r build
